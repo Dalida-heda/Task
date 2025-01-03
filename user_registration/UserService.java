03dd79e5380dbfc54070ffd7452bbcdf1f82eb05
Dalia Wahdan <150842212+Dalida-heda@users.noreply.github.com>
2025-01-03 14:44:14 +0200

    Update UserService.java

--- user_registration/UserService.java
+++ user_registration/UserService.java
@@ -5,24 +5,24 @@ import java.util.List;
 public class UserService {
     private List<User> users = new ArrayList<>();
 
-    // register a new user
+   
     public void registerUser (String username, String email, String password) {
         User newUser  = new User(username, email, password);
         users.add(newUser );
         System.out.println("User  registered successfully!");
     }
 
-    // find a user by email
+ 
     public User findUserByEmail(String email) {
         for (User  user : users) {
             if (user.getEmail().equals(email)) {
                 return user;
             }
         }
-        return null; // User not found
+        return null;
     }
 
-    // log in a user
+ 
     public boolean loginUser (String email, String password) {
         User user = findUserByEmail(email);
         if (user != null && user.getPassword().equals(password)) {
